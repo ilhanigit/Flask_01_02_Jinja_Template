e98a3f9f84866e6c17934cae28a892974fe1f6e8
Abraham <ibrahimilhan1999@gmail.com>
2024-07-01 22:55:33 +0300

    new files added

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "hello word"
+
+@app.route("/second")
+def head2():
+    return "second page"
+
+@app.route("/third")
+def head3():
+    return "third page"
 
 if __name__ == '__main__':
 
